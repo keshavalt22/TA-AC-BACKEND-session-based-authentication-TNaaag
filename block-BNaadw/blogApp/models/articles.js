01a1ccc160aec63afdabc084c95854cbc0a25291
keshavalt22 <keshavkhatri35@gmail.com>
2022-06-21 22:42:24 +0530

    assignment

--- block-BNaadw/blogApp/models/articles.js
+++ block-BNaadw/blogApp/models/articles.js
@@ -5,8 +5,8 @@ let articleSchema = new Schema({
     title: {type: String, required: true},
     description: {type: String},
     likes: {type: Number, default: 0},
-    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-    author: String
+    author: String,
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }]
 }, {timestamps: true});
 
 module.exports = mongoose.model('Article', articleSchema);
